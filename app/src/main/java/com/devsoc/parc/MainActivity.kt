5236ab1651a3ae5bb14d6c67fd4918e448eb3f40
Shivam <shivampachchigar14112@gmail.com>
2022-06-12 22:53:18 +0530

    Fixed bugs, added DataBinding

--- app/src/main/java/com/devsoc/parc/MainActivity.kt
+++ app/src/main/java/com/devsoc/parc/MainActivity.kt
@@ -2,10 +2,17 @@ package com.devsoc.parc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.devsoc.parc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val view = binding.root
+        setContentView(view)
     }
 }
---
+++ app/src/main/java/com/devsoc/parc/SecondFragment.kt
@@ -0,0 +1,28 @@
+package com.devsoc.parc
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.devsoc.parc.databinding.FragmentSecondBinding
+
+class secondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //TODO: Add the logic code here
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
