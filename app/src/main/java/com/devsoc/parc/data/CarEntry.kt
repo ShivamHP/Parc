37653aafc3272e8e506fbabc002e77c8486162b8
ShivamHP <shivampachchigar14112@gmail.com>
2022-06-15 14:11:11 +0530

    Minor bug fixes

---
+++ app/src/main/java/com/devsoc/parc/data/CarEntry.kt
@@ -0,0 +1,8 @@
+package com.devsoc.parc.data
+
+data class CarEntry(
+    val carNumber: String = "",
+    val phoneNumber: Int = 0,
+    val parkingSpaceAllotted: String = "",
+    val imgPath: String = ""
+)
